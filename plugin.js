dc7fd3d6d04cc50ccc62129c30c50cfc97fdd80a
Rémi Becheras <rbecheras@sirap.fr>
2016-05-03 23:59:30 +0200

    reorganize and clean all the plugin code thanks to standard code style

--- plugin.js
+++ plugin.js
@@ -8,43 +8,31 @@
  * Contributing: http://www.tinymce.com/contributing
  */
 
+// browser globals
 var MutationObserver = window.MutationObserver
 var $ = window.$
 var tinymce = window.tinymce
 
-tinymce.PluginManager.add('menuscontroller', function (editor) {
-  function observebodyMutations () {
-    // var targetDOMQuery = '#mce-plugin-headersfooters-remove-header'
-    var menubarMenuDOMQuery = '.mce-container.mce-panel.mce-floatpanel.mce-menu'
+// register the createPlugin function
+tinymce.PluginManager.add('menuscontroller', createPlugin)
 
-    // select the target node
-    var target = document.querySelector('body')
+function createPlugin (editor) {
+  // expose the plugin API
+  // example of usage:
+  // var mceMenubar = editor.plugins.menuscontroller.getMenubar()
+  this.getMenubar = getMenubar
+  this.getMenuByName = getMenuByName
+  this.getToolbars = getToolbars
 
-    // create an observer instance
-    var observer = new MutationObserver(function (mutations) {
-      mutations.forEach(function (mutation) {
-        // search menu with menubarMenuDOMQuery
-        var menus = $(mutation.addedNodes).filter(menubarMenuDOMQuery)
-        if (menus.length) {
-          if (menus.length > 1) throw new Error('ERROR: there is many menus rendered ! This case is not implemented !')
-          // trigger an app:mceMenuRendered event with a 'evt.data.menu' property
-          var menuRenderedEvent = $.Event('app:mceMenuRendered')
-          $('body').trigger(menuRenderedEvent, menus[0])
-        }
-      })
+  // trigger an event 'app:mceMenuItemRendered:<menu_item_id>' for each menu item when its parent menu is rendered
+  $('body').on('app:mceMenuRendered', function (evt, menu) {
+    $(menu).find('.mce-menu-item').each(function (i, menuItem) {
+      $(menu).trigger('app:mceMenuItemRendered:' + $(menuItem).attr('id'))
     })
+  })
 
-    // configuration of the observer:
-    var config = { childList: true }
-
-    // pass in the target node, as well as the observer options
-    observer.observe(target, config)
-
-  // later, you can stop observing
-  // $timeout(function(){
-  // 	observer.disconnect()
-  // },10000)
-  }
+  // start to observe menus rendering
+  observeMenuRendering()
 
   function getMenubar () {
     var themeItems = editor.theme.panel.settings.items
@@ -75,20 +63,34 @@ tinymce.PluginManager.add('menuscontroller', function (editor) {
       if (item.type === 'panel' && item.classes === 'toolbar-grp') return item.items
     }
   }
+}
 
-  $('body').on('app:mceMenuRendered', function (evt, menu) {
-    $(menu).find('.mce-menu-item').each(function (i, menuItem) {
-      $(menu).trigger('app:mceMenuItemRendered:' + $(menuItem).attr('id'))
+function observeMenuRendering () {
+  // var targetDOMQuery = '#mce-plugin-headersfooters-remove-header'
+  var menubarMenuDOMQuery = '.mce-container.mce-panel.mce-floatpanel.mce-menu'
+  // select the target node
+  var target = document.querySelector('body')
+  // create an observer instance
+  var observer = new MutationObserver(bodyMutationsCallback)
+  // configuration of the observer:
+  var config = { childList: true }
+
+  // pass in the target node, as well as the observer options
+  // later, we can stop observing with observer.disconnect()
+  observer.observe(target, config)
+
+  function bodyMutationsCallback (mutations) {
+    mutations.forEach(function (mutation) {
+      // search menu with menubarMenuDOMQuery
+      var menus = $(mutation.addedNodes).filter(menubarMenuDOMQuery)
+      if (menus.length) {
+        // menus are renders once at time
+        if (menus.length > 1) throw new Error('ERROR: there is many menus rendered ! This case is not implemented !')
+
+        // trigger an 'app:mceMenuRendered' event with a 'evt.data.menu' property
+        var menuRenderedEvent = $.Event('app:mceMenuRendered')
+        $('body').trigger(menuRenderedEvent, menus[0])
+      }
     })
-  })
-
-  // start to observe menus rendering
-  observebodyMutations()
-
-  // exports the API to the 'menuscontroller' plugin
-  // example:
-  // var mceMenubar = editor.plugins.menuscontroller.getMenubar()
-  this.getMenubar = getMenubar
-  this.getMenuByName = getMenuByName
-  this.getToolbars = getToolbars
-})
+  }
+}
