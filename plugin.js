b0f4ab33994e08a4fec9b0935e3b5d91565e2076
Rémi Becheras <rbecheras@sirap.fr>
2016-05-03 23:38:14 +0200

    reformat plugin with standard style guide

--- plugin.js
+++ plugin.js
@@ -8,87 +8,87 @@
  * Contributing: http://www.tinymce.com/contributing
  */
 
-/*global tinymce:true */
+var MutationObserver = window.MutationObserver
+var $ = window.$
+var tinymce = window.tinymce
 
-tinymce.PluginManager.add('menuscontroller', function(editor) {
+tinymce.PluginManager.add('menuscontroller', function (editor) {
+  function observebodyMutations () {
+    // var targetDOMQuery = '#mce-plugin-headersfooters-remove-header'
+    var menubarMenuDOMQuery = '.mce-container.mce-panel.mce-floatpanel.mce-menu'
 
-  function observebodyMutations(){
-  	var targetDOMQuery = '#mce-plugin-headersfooters-remove-header';
-  	var menubarMenuDOMQuery = '.mce-container.mce-panel.mce-floatpanel.mce-menu';
+    // select the target node
+    var target = document.querySelector('body')
 
-  	// select the target node
-  	var target = document.querySelector('body');
+    // create an observer instance
+    var observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        // search menu with menubarMenuDOMQuery
+        var menus = $(mutation.addedNodes).filter(menubarMenuDOMQuery)
+        if (menus.length) {
+          if (menus.length > 1) throw new Error('ERROR: there is many menus rendered ! This case is not implemented !')
+          // trigger an app:mceMenuRendered event with a 'evt.data.menu' property
+          var menuRenderedEvent = $.Event('app:mceMenuRendered')
+          $('body').trigger(menuRenderedEvent, menus[0])
+        }
+      })
+    })
 
-  	// create an observer instance
-  	var observer = new MutationObserver(function(mutations) {
-  		mutations.forEach(function(mutation) {
-  			// search menu with menubarMenuDOMQuery
-  			var menus = $(mutation.addedNodes).filter(menubarMenuDOMQuery);
-  			if (menus.length) {
-  				if (menus.length > 1) throw new Error('ERROR: there is many menus rendered ! This case is not implemented !');
-  				// trigger an app:mceMenuRendered event with a 'evt.data.menu' property
-  				var menuRenderedEvent = $.Event('app:mceMenuRendered');
-  				$('body').trigger(menuRenderedEvent, menus[0]);
-  			}
-  		});
-  	});
+    // configuration of the observer:
+    var config = { childList: true }
 
-  	// configuration of the observer:
-  	var config = { childList: true };
+    // pass in the target node, as well as the observer options
+    observer.observe(target, config)
 
-  	// pass in the target node, as well as the observer options
-  	observer.observe(target, config);
-
-  	// later, you can stop observing
-  	// $timeout(function(){
-  	// 	observer.disconnect();
-  	// },10000);
+  // later, you can stop observing
+  // $timeout(function(){
+  // 	observer.disconnect()
+  // },10000)
   }
 
-  function getMenubar(){
-    var themeItems = editor.theme.panel.settings.items;
+  function getMenubar () {
+    var themeItems = editor.theme.panel.settings.items
     for (var i = 0; i < themeItems.length; i++) {
-      var item = themeItems[i];
-      if (item.type === 'menubar') return item.items;
+      var item = themeItems[i]
+      if (item.type === 'menubar') return item.items
     }
   }
 
-  function getMenuByName(name){
+  function getMenuByName (name) {
     try {
-      var menubar = getMenubar();
+      var menubar = getMenubar()
       for (var i = 0; i < menubar.length; i++) {
-        var menu = menubar[i];
+        var menu = menubar[i]
         if (menu.text.toLowerCase() === name.toLowerCase()) {
-          return menu;
+          return menu
         }
       }
-    } catch(e) {
-      throw new Error('Can\'t find the menu ' + name + ' in menu bar');
+    } catch (e) {
+      throw new Error("Can't find the menu " + name + ' in menu bar')
     }
   }
 
-  function getToolbars(){
-    var themeItems = editor.theme.panel.settings.items;
+  function getToolbars () {
+    var themeItems = editor.theme.panel.settings.items
     for (var i = 0; i < themeItems.length; i++) {
-      var item = themeItems[i];
-      if (item.type === 'panel' && item.classes === 'toolbar-grp') return item.items;
+      var item = themeItems[i]
+      if (item.type === 'panel' && item.classes === 'toolbar-grp') return item.items
     }
   }
 
-  $('body').on('app:mceMenuRendered',function(evt, menu){
-  	$(menu).find('.mce-menu-item').each(function(i, menuItem){
-  		$(menu).trigger('app:mceMenuItemRendered:' + $(menuItem).attr('id'));
-  	});
-  });
+  $('body').on('app:mceMenuRendered', function (evt, menu) {
+    $(menu).find('.mce-menu-item').each(function (i, menuItem) {
+      $(menu).trigger('app:mceMenuItemRendered:' + $(menuItem).attr('id'))
+    })
+  })
 
   // start to observe menus rendering
-  observebodyMutations();
+  observebodyMutations()
 
   // exports the API to the 'menuscontroller' plugin
   // example:
   // var mceMenubar = editor.plugins.menuscontroller.getMenubar()
-  this.getMenubar = getMenubar;
-  this.getMenuByName = getMenuByName;
-  this.getToolbars = getToolbars;
-
-});
+  this.getMenubar = getMenubar
+  this.getMenuByName = getMenuByName
+  this.getToolbars = getToolbars
+})
