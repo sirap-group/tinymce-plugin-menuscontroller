48c64ba7fc6cbfd0027b3a17892ddc5c95a09b10
Rémi Becheras <rbecheras@sirap.fr>
2016-05-04 12:17:31 +0200

    standardize gruntfile and add gruntfile to the standard task

--- gruntfile.js
+++ gruntfile.js
@@ -1,57 +1,59 @@
-
 module.exports = function (grunt) {
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt)
 
-    grunt.initConfig({
-      standard: {
-        options: {
-          format: true
-        },
-        js: {
-          src: ['plugin.js']
-        }
+  grunt.initConfig({
+    standard: {
+      options: {
+        format: true
       },
-      uglify: {
-        dist: {
-          files: {
-            'plugin.min.js': ['plugin.js']
-          }
-        }
+      gruntfile: {
+        src: ['gruntfile.js']
       },
-      watch: {
-        // package:  {
-        //   files: 'package.json',
-        //   options: { reload: true }
-        // },
-        // gruntfile: {
-        //   files: 'gruntfile.js',
-        //   tasks: ['jshint:gruntfile'],
-        // },
-        js: {
-          files: ['plugin.js'],
-          tasks: ['standard:js'],
+      js: {
+        src: ['plugin.js']
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'plugin.min.js': ['plugin.js']
         }
-      },
-      bump: {
-        options: {
-          files: ['package.json','bower.json'],
-          updateConfigs: [],
-          commit: true,
-          commitMessage: 'Release v%VERSION%',
-          commitFiles: ['package.json','bower.json'],
-          createTag: true,
-          tagName: 'v%VERSION%',
-          tagMessage: 'Version %VERSION%',
-          push: true,
-          pushTo: 'origin',
-          gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-          globalReplace: false,
-          prereleaseName: false,
-          regExp: false
-        }
-      },
-    });
+      }
+    },
+    watch: {
+      // package:  {
+      //   files: 'package.json',
+      //   options: { reload: true }
+      // },
+      // gruntfile: {
+      //   files: 'gruntfile.js',
+      //   tasks: ['jshint:gruntfile'],
+      // },
+      js: {
+        files: ['plugin.js'],
+        tasks: ['standard:js']
+      }
+    },
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json', 'bower.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false,
+        prereleaseName: false,
+        regExp: false
+      }
+    }
+  })
 
-    grunt.registerTask('default', ['standard','watch']);
-    grunt.registerTask('build', ['standard','uglify']);
-};
+  grunt.registerTask('default', ['standard', 'watch'])
+  grunt.registerTask('build', ['standard', 'uglify'])
+}
